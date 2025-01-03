4a3571b56d55d3268742cd2559c62bccc054f3c7
GodtoL <eliasvera793@gmail.com>
2025-01-03 17:46:43 -0300

    Se agrega la sección principal

---
+++ src/MainSection.jsx
@@ -0,0 +1,35 @@
+export default function MainSection() {
+    return (
+        <>
+            <main>
+                <h1>Recomendaciones de libros</h1>
+                <form>
+                    <CreateBookLabel propName="Titulo" />
+                    <CreateBookLabel propName="Autor" />
+                    <CreateBookLabel propName="Descripción breve" />
+                    <label>Sinopsis:</label>
+                    <textarea required></textarea>
+                    <label>Etiqueta:</label>
+                    <select required>
+                        <option value="" disabled selected>
+                            Selecciona un tag
+                        </option>
+                        <option value="ficcion">Ficción</option>
+                        <option value="no-ficcion">No Ficción</option>
+                        <option value="misterio">Misterio</option>
+                    </select>
+                    <button type="submit">Crear Recomendación</button>
+                </form>
+            </main>
+        </>
+    );
+}
+
+function CreateBookLabel({ propName }) {
+    return (
+        <>
+            <label>{propName}:</label>
+            <input type="text" name={propName.toLowerCase().replace(/\s/g, "-")} required />
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Header from './Header.jsx'
+import MainSection from './MainSection.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header></Header>
+    <MainSection></MainSection>
   </StrictMode>,
 )
